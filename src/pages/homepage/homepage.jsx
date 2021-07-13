3105a7b94312ace27e7601ccfb7c88c598bba3b9
Shivay10th <62726735+Shivay10th@users.noreply.github.com>
2021-07-13 12:19:25 +0530

    Homepage dev Start

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react';
+
+const HomePage = () => (
+	<React.Fragment>
+		<h1>HomePage</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+			voluptatum iste assumenda provident deserunt. Nihil facilis quas
+			libero voluptas hic deleniti, fuga est! Quis nam, veritatis sunt
+			illo voluptatem ut.
+		</p>
+	</React.Fragment>
+);
+
+export default HomePage;
